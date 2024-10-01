710d064ceffc91948396ed98b700deb8893f2d86
Ray Thomas <rthomas5678@gmail.com>
2024-09-30 21:27:33 -0700

    added env variable for API Key

--- client/src/components/Current.jsx
+++ client/src/components/Current.jsx
@@ -1,5 +1,6 @@
 import {useState ,useEffect} from 'react'
 import axios from 'axios'
+// const apiKey = import.meta.env.API_KEY;
 
 // a20a289672fbb48069f510904d9915b7
 
@@ -11,11 +12,11 @@ export default function Current() {
     const fetchCityCoords = async () => {
         try {
             //GEOCODING LOCATION NAME TO GET COORDINATES
-            const coords = await axios.get('http://api.openweathermap.org/geo/1.0/direct?q=Phoenix&limit=5&appid=a20a289672fbb48069f510904d9915b7&units=imperial');
+            const coords = await axios.get(`http://api.openweathermap.org/geo/1.0/direct?q=Phoenix&limit=5&appid=${import.meta.env.VITE_API_KEY}&units=imperial`);
             console.log(coords.data)
 
             //USING LAT AND LON TO GET CITY WEATHER DATA
-            const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${coords.data[0].lat}&lon=${coords.data[0].lon}&appid=a20a289672fbb48069f510904d9915b7&units=imperial`)
+            const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${coords.data[0].lat}&lon=${coords.data[0].lon}&appid=${import.meta.env.VITE_API_KEY}&units=imperial`)
             console.log(response.data)
             setLocation(response.data)
             setMain(response.data.main)
